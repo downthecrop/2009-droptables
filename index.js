a9f70b803622950aeb37648f88e05abf03f1f393
downthecrop <bryce@mathx.ca>
2021-10-13 21:53:36 -0700

    Faster searches, big bugfixes

--- index.js
+++ index.js
@@ -2,131 +2,153 @@ var npcG
 var dropG
 var itemG
 var debugClass = "debug"
+var indexedDrops = {}
 
-function test(e){
-    let value = e.value.toLowerCase().replace(' ','')
+function sanitize(str){
+    return str.replace(' ','').toLowerCase()
+}
+
+function genDropIndex(dropTable){
+    for (let i = 0; i < dropTable.length; i += 1){
+        dropTable[i]['ids'].split(",").forEach(id => {
+            indexedDrops[id] = i
+        })
+    }
+}
+
+function search(e){
+    let searchStr = sanitize(e.value)
+    console.log(searchStr)
     let table = document.getElementById("table")
     table.innerHTML = ""
     //Search for matching ID
-    Object.keys(npcG).forEach(function(key) {
-        if (value.length > 1 && key.toLowerCase().replace(' ','').includes(value)){
-            for (let i = 0; i < dropG.length; i += 1){
-                if (npcG[key].includes(dropG[i]['ids'])){
+    Object.keys(npcG).forEach(function(npcName) {
+        if (searchStr.length > 2 && sanitize(npcName).includes(searchStr)){
+            console.log(searchStr + " is like "+npcName)
+            var tblBody = document.createElement("tbody");
+            
+            let npcID = npcG[npcName].split(",")
 
-                    // creates a <table> element and a <tbody> element
-                    var tblBody = document.createElement("tbody");
+            let flag = false
 
-                    // Get the combined weight of everything for later
-                    // Iterating the loop 2 times.. maybe don't do this at home.
-                    let totalWeight = 0.0;
-                    for (let j = 0; j < dropG[i]['main'].length; j += 1){
-                        totalWeight += parseFloat(dropG[i]['main'][j]["weight"])
+            npcID.every(id => {
+                if(indexedDrops[id] !== undefined){
+                    flag = true;
+                    i = indexedDrops[id]
+                    return true
+                }
+            })
+            if (flag){
+                // Get the combined weight of everything for later
+                // Iterating the loop 2 times.. maybe don't do this at home.
+                let totalWeight = 0.0;
+                for (let j = 0; j < dropG[i]['main'].length; j += 1){
+                    totalWeight += parseFloat(dropG[i]['main'][j]["weight"])
+                }
+                
+                // Guarantee/Default Droptable Items
+                for (let j = 0; j < dropG[i]['default'].length; j += 1){
+                    var row = document.createElement("tr");
+                    var cell = document.createElement("td");
+
+                    // Icon Cell
+                    const itemIcon = document.createElement("img");
+                    itemIcon.src = "./items-icons/" + dropG[i]['default'][j]["id"] + ".png"
+                    cell.appendChild(itemIcon);
+                    row.appendChild(cell)
+
+                    // Item Name Cell
+                    var cell = document.createElement("td");
+                    var cellText = document.createTextNode(itemG[dropG[i]['default'][j]["id"]]);
+                    var debugDiv = document.createElement('div');
+                    var debugText = document.createTextNode("id: "+dropG[i]['default'][j]["id"]);
+                    debugDiv.className = debugClass
+                    debugDiv.appendChild(debugText)
+                    cell.appendChild(cellText);
+                    cell.appendChild(debugDiv);
+                    row.append(cell)
+
+                    // Quantity
+                    var cell = document.createElement("td");
+                    if (dropG[i]['default'][j]["minAmount"] != dropG[i]['default'][j]["maxAmount"]){
+                        amount = dropG[i]['default'][j]["minAmount"] + "-" + dropG[i]['default'][j]["maxAmount"]
+                    } else {
+                        amount = dropG[i]['default'][j]["minAmount"]
                     }
-                    
-                    // Guarantee/Default Droptable Items
-                    for (let j = 0; j < dropG[i]['default'].length; j += 1){
-                        var row = document.createElement("tr");
-                        var cell = document.createElement("td");
+                    var cellText = document.createTextNode(amount);
+                    cell.appendChild(cellText);
+                    row.append(cell)
 
-                        // Icon Cell
-                        const itemIcon = document.createElement("img");
-                        itemIcon.src = "./items-icons/" + dropG[i]['default'][j]["id"] + ".png"
-                        cell.appendChild(itemIcon);
-                        row.appendChild(cell)
+                    // Rarity (Always)
+                    var cell = document.createElement("td");
+                    var cellText = document.createTextNode("Always");
+                    cell.className = "always"
+                    cell.appendChild(cellText);
+                    row.append(cell)
 
-                        // Item Name Cell
-                        var cell = document.createElement("td");
-                        var cellText = document.createTextNode(itemG[dropG[i]['default'][j]["id"]]);
-                        var debugDiv = document.createElement('div');
-                        var debugText = document.createTextNode("id: "+dropG[i]['default'][j]["id"]);
-                        debugDiv.className = debugClass
-                        debugDiv.appendChild(debugText)
-                        cell.appendChild(cellText);
-                        cell.appendChild(debugDiv);
-                        row.append(cell)
+                    // Append the TD items to the Row
+                    tblBody.appendChild(row);
+                }
 
-                        // Quantity
-                        var cell = document.createElement("td");
-                        if (dropG[i]['default'][j]["minAmount"] != dropG[i]['default'][j]["maxAmount"]){
-                            amount = dropG[i]['default'][j]["minAmount"] + "-" + dropG[i]['default'][j]["maxAmount"]
-                        } else {
-                            amount = dropG[i]['default'][j]["minAmount"]
-                        }
-                        var cellText = document.createTextNode(amount);
-                        cell.appendChild(cellText);
-                        row.append(cell)
 
-                        // Rarity (Always)
-                        var cell = document.createElement("td");
-                        var cellText = document.createTextNode("Always");
+                // Normal Droptable Items
+                for (let j = 0; j < dropG[i]['main'].length; j += 1){
+                    if(dropG[i]['main'][j]["id"] == "0")
+                        continue
+                    var row = document.createElement("tr");
+                    var cell = document.createElement("td");
+
+                    // Icon Cell
+                    const itemIcon = document.createElement("img");
+                    itemIcon.src = "./items-icons/" + dropG[i]['main'][j]["id"] + ".png"
+                    cell.appendChild(itemIcon);
+                    row.appendChild(cell)
+
+                    // Item Name Cell
+                    var cell = document.createElement("td");
+                    var cellText = document.createTextNode(itemG[dropG[i]['main'][j]["id"]]);
+                    var debugDiv = document.createElement('div');
+                    var debugText = document.createTextNode("id: "+dropG[i]['main'][j]["id"]);
+                    debugDiv.className = debugClass
+                    debugDiv.appendChild(debugText)
+                    cell.appendChild(cellText);
+                    cell.appendChild(debugDiv)
+                    row.append(cell)
+
+                    // Quantity
+                    var cell = document.createElement("td");
+                    if (dropG[i]['main'][j]["minAmount"] != dropG[i]['main'][j]["maxAmount"]){
+                        amount = dropG[i]['main'][j]["minAmount"] + "-" + dropG[i]['main'][j]["maxAmount"]
+                    } else {
+                        amount = dropG[i]['main'][j]["minAmount"]
+                    }
+                    var cellText = document.createTextNode(amount);
+                    cell.appendChild(cellText);
+                    row.append(cell)
+
+                    // Rarity
+                    let weight = parseFloat(dropG[i]['main'][j]["weight"])
+                    var cell = document.createElement("td");
+                    var chance = (weight/totalWeight)*100
+                    // Remove trailing zeros (Tried a bunch of stuff and couldn't get it out without)
+                    var cellText = document.createTextNode("1/"+(+parseFloat(100/chance).toFixed(2).replace(/(\.0+|0+)$/, '')));
+                    if (chance > 99.99){
                         cell.className = "always"
-                        cell.appendChild(cellText);
-                        row.append(cell)
-
-                        // Append the TD items to the Row
-                        tblBody.appendChild(row);
+                    } else if (chance > 4){
+                        cell.className = "common"
+                    } else if (chance > 1) {
+                        cell.className = "uncommon"
+                    } else if (chance > 0.1){
+                        cell.className = "rare"
+                    } else {
+                        cell.className = "veryrare"
                     }
+                    cell.title = parseFloat((chance).toFixed(2)) + "%"
+                    cell.appendChild(cellText);
+                    row.append(cell)
 
-
-                    // Normal Droptable Items
-                    for (let j = 0; j < dropG[i]['main'].length; j += 1){
-                        if(dropG[i]['main'][j]["id"] == "0")
-                            continue
-                        var row = document.createElement("tr");
-                        var cell = document.createElement("td");
-
-                        // Icon Cell
-                        const itemIcon = document.createElement("img");
-                        itemIcon.src = "./items-icons/" + dropG[i]['main'][j]["id"] + ".png"
-                        cell.appendChild(itemIcon);
-                        row.appendChild(cell)
-
-                        // Item Name Cell
-                        var cell = document.createElement("td");
-                        var cellText = document.createTextNode(itemG[dropG[i]['main'][j]["id"]]);
-                        var debugDiv = document.createElement('div');
-                        var debugText = document.createTextNode("id: "+dropG[i]['main'][j]["id"]);
-                        debugDiv.className = debugClass
-                        debugDiv.appendChild(debugText)
-                        cell.appendChild(cellText);
-                        cell.appendChild(debugDiv)
-                        row.append(cell)
-
-                        // Quantity
-                        var cell = document.createElement("td");
-                        if (dropG[i]['main'][j]["minAmount"] != dropG[i]['main'][j]["maxAmount"]){
-                            amount = dropG[i]['main'][j]["minAmount"] + "-" + dropG[i]['main'][j]["maxAmount"]
-                        } else {
-                            amount = dropG[i]['main'][j]["minAmount"]
-                        }
-                        var cellText = document.createTextNode(amount);
-                        cell.appendChild(cellText);
-                        row.append(cell)
-
-                        // Rarity
-                        let weight = parseFloat(dropG[i]['main'][j]["weight"])
-                        var cell = document.createElement("td");
-                        var chance = (weight/totalWeight)*100
-                        // Remove trailing zeros (Tried a bunch of stuff and couldn't get it out without)
-                        var cellText = document.createTextNode("1/"+(+parseFloat(100/chance).toFixed(2).replace(/(\.0+|0+)$/, '')));
-                        if (chance > 99.99){
-                            cell.className = "always"
-                        } else if (chance > 4){
-                            cell.className = "common"
-                        } else if (chance > 1) {
-                            cell.className = "uncommon"
-                        } else if (chance > 0.1){
-                            cell.className = "rare"
-                        } else {
-                            cell.className = "veryrare"
-                        }
-                        cell.title = parseFloat((chance).toFixed(2)) + "%"
-                        cell.appendChild(cellText);
-                        row.append(cell)
-
-                        // Append the TD items to the Row
-                        tblBody.appendChild(row);
-                    }
+                    // Append the TD items to the Row
+                    tblBody.appendChild(row);
                 }
             }
             if (tblBody.innerText){
@@ -134,12 +156,11 @@ function test(e){
                 let debugDiv = document.createElement('div')
                 debugDiv.className = debugClass
                 var debugText = document.createElement('p');
-                debugText.innerHTML = ("ids: "+npcG[key]);
+                debugText.innerHTML = ("ids: "+npcG[npcName]);
                 debugDiv.appendChild(debugText)
-                h1.innerText = key
+                h1.innerText = npcName
                 table.appendChild(h1)
                 table.appendChild(debugDiv)
-
                 document.getElementById("table").appendChild(tblBody)
             }
         }
@@ -192,13 +213,14 @@ window.addEventListener('load', (event) => {
     let counter = 0;
     let checkExist = setInterval(function () {
         if (dropG != undefined && npcG != undefined && itemG != undefined) {
-            document.getElementsByClassName("loading")[0].setAttribute("style","display:none;")
             clearInterval(checkExist);
+            document.getElementsByClassName("loading")[0].setAttribute("style","display:none;")
+            genDropIndex(dropG)
 
             //Allow users to directly link a table
             if(window.location.search){
                 document.getElementsByTagName("input")[0].value = window.location.search.substring(1)
-                test(document.getElementsByTagName("input")[0])
+                search(document.getElementsByTagName("input")[0])
             }
         }
         if (counter > 6)
